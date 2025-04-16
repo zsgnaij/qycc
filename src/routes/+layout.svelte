e1ee3403282e5430d043e6d0ff63365cf9fd86f6
zsgnaij <18057950097@163.com>
2025-04-16 10:40:13 +0800

    rollback

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
 	import '../app.css';
 	import { base } from '$app/paths';
 
+	export const ssr = false;
 	let { children } = $props();
 </script>
-
 <nav>
 	<a href="{base}/">home</a>
 	<a href="{base}/about">about</a>
