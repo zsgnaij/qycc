70a2e19fa54d5aae9bb1888f0a3e14a1726279da
zsgnaij <18057950097@163.com>
2025-04-16 10:52:42 +0800

    fix trailingSlash

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	import { base } from '$app/paths';
-	
+
 	export const prerender = true;
+	export const trailingSlash = 'always';
 
 	let { children } = $props();
 </script>
